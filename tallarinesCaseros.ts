c1610f9216a193f56ab4aea85f37cae7e93c8242
lucasquinones80 <ljq022@gmail.com>
2023-03-20 19:30:28 -0300

    tallarinesCaseros

---
+++ tallarinesCaseros.ts
@@ -0,0 +1,10 @@
+console.log("tamizar la harina y formar una corna");
+console.log(" batir los huevos e incorporara a la harina");
+console.log(" amasar hasta obtener una masa lisa");
+console.log("dejar reposar 30 minutos");
+console.log("dividir la masa en 4 bollos");
+console.log("dejar airear la masa durante 15 minutos");
+console.log("enrrollara la masa sobre si misma");
+console.log("cortar la masa de ancho que mas guste y colocar sobre una bandeja");
+console.log("herviren abundante agua");
+console.log("escurrir muy bien e incorporar la salsa que mas le guste");
